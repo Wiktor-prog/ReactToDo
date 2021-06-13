7c82eb5f3feaff4fe0a5df7d41f72ec5ca2ee7f5
Wiktor Ziejewski <wiktor.ziejewski>
2021-06-13 20:42:10 +0200

    Header 13.2

---
+++ src/components/Container/Container.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Container.scss';
+import PropTypes from 'prop-types';
+
+const Container = props => (
+
+  <div className={styles.component}>
+    {props.childern} 
+  </div>   
+);
+
+Container.propTypes = {
+  childern: PropTypes.node,
+};
+
+export default Container;
+            
